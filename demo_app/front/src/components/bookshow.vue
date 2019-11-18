ad74ca356ba35016f5f4b189de34af5fd525d7f7
Ronnie <1017268540@qq.com>
2019-11-18 21:02:29 +0800

    搜索页面

---
+++ demo_app/front/src/components/bookshow.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+
+      </div>
+      <div class="col-md-8">
+         <div class="panel panel-default">
+          <div class="panel-heading">
+            <h5 class="panel-title">books</h5>
+          </div>
+          <div class="panel-body">
+
+          </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default{
+      name:'mybook',
+      data(){
+          return{
+
+          }
+      }
+  }
+</script>
+<style scoped>
+
+</style>
