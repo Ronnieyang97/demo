fd66e33dcb9361970b3fc3f0200989e6db50c004
Ronnie <1017268540@qq.com>
2019-11-18 17:20:49 +0800

    搭建vue框架

---
+++ demo_app/front/src/components/mybook.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mabook">
+    我的图书
+
+  </div>
+
+</template>
+
+<script>
+  export default{
+      name:'mybook',
+      data(){
+          return{
+
+          }
+      }
+  }
+</script>
+<style scoped>
+
+</style>
---
+++ demo_app/front/src/components/vmain.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+    首页
+  </div>
+
+</template>
+
+<script>
+  export default{
+      name:'vmain',
+      data(){
+          return{
+
+          }
+      }
+  }
+</script>
+<style scoped>
+
+</style>
